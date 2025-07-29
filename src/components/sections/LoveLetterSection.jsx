8175d141da1383b2ddce51d131cd74938206fd9f
Saimoan <iderrillix@gmail.com>
2025-07-30 03:35:03 +0800

    initial content to love letter section

--- src/components/sections/LoveLetterSection.jsx
+++ src/components/sections/LoveLetterSection.jsx
@@ -7,7 +7,19 @@ function LoveLetterSection() {
             Love Letter
         </div>
         <span className='text-xl font-medium text-center text-gray-600 mt-2'>Words I wish I say more often.</span>
-        <div className='bg-white rounded-md w-[600px] h-[600px]'></div>
+        <div className='bg-white rounded-md w-[600px] h-[600px] p-4 flex flex-col gap-1'>
+          <span>To my dear girlfriend,</span>
+          <br />
+          <span>Happy girlfriend's day!</span>
+          <br />
+          <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, velit animi? Expedita molestiae, aliquam consequatur tenetur ipsa fugit. Veritatis dignissimos iure dolor distinctio debitis voluptatibus repellendus amet ea illo quas.</span>
+          <br />
+          <span>Here’s to many more adventures, sweet moments, and memories together. 💖</span>
+          <br />
+          <span>Love,</span>
+          <br />
+          <span>Your bf </span>
+        </div>
     </div>
   )
 }
